4332cb5e8b1f7ea1cd7eb9c01e357e9c5383a813
Célio Júnior <juniorcelioata@gmail.com>
2025-03-27 13:06:13 -0300

    Atualizando projeto.

--- src/exception/MenuException.java
+++ src/exception/MenuException.java
@@ -9,7 +9,7 @@ public class MenuException extends RuntimeException {
         try {
             return Integer.parseInt(entrada);
         } catch (NumberFormatException e) {
-            throw new MenuException("Opção inválida! Digite um número de 0 a 4.");
+            throw new MenuException("Opção inválida! Digite um número valido.");
         }
     }
 }
